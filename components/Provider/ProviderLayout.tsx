491fc89e62d33c0fa4d319572f0b3db6dec6f759
Antonette Caldwell <pullmana8@gmail.com>
2023-02-27 19:54:23 -0600

    Created Provider layout and it's children
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

--- components/Provider/ProviderLayout.tsx
+++ components/Provider/ProviderLayout.tsx
@@ -1,4 +1,3 @@
-import NoSsr from '@mui/material/NoSsr';
 import React, {type ReactNode} from 'react';
 
 type ProviderLayoutProps = {
@@ -8,11 +7,7 @@ type ProviderLayoutProps = {
 export default function ProviderLayout({children}: ProviderLayoutProps) {
 	return (
 		<>
-			<NoSsr>
-				<div data-cy='root' style={{padding: '170px 0px', textAlign: 'center'}}>
-					{children}
-				</div>
-			</NoSsr>
+			{children}
 		</>
 	);
 }

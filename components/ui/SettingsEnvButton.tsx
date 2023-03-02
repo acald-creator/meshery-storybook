b1f37c4a4bb5a766f57137c8bc18703a109c44c5
Antonette Caldwell <pullmana8@gmail.com>
2023-03-02 10:01:16 -0600

    Created Settings environment button
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

---
+++ components/ui/SettingsEnvButton.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function MesherySettingsEnvButton() {
+	return (
+		<Button>
+			<Typography>Add Cluster</Typography>
+		</Button>
+	);
+}

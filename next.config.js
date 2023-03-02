b3c15e7c4775a70e3141bb5ecd6d2e809a665b88
Antonette Caldwell <pullmana8@gmail.com>
2023-03-02 10:09:14 -0600

    Formatted next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/api',
-        destination: 'http://localhost:9081/api',
-        permanent: false,
-      },
-    ]
-  },
-}
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/api',
+				destination: 'http://localhost:9081/api',
+				permanent: false,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

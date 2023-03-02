ddee97e212d5353ba7933fae3bb706976a4deccf
Antonette Caldwell <pullmana8@gmail.com>
2023-03-02 10:08:28 -0600

    Added type to define Provider

---
+++ lib/providers/types.ts
@@ -0,0 +1,26 @@
+import {type EmotionCache} from '@emotion/react';
+import {type ReactElement} from 'react';
+
+export type AppThemeProviderProps = {
+	children: ReactElement;
+	emotionCache?: EmotionCache;
+};
+
+export type UserDataType = {
+	id: string;
+	user_id: string;
+	first_name: string;
+	last_name: string;
+	avatar_url?: string | undefined;
+	provider: string;
+	email: string;
+	setAvailableProviders: string | undefined;
+};
+
+export type AuthValuesType = {
+	user: UserDataType | undefined;
+	loading: boolean;
+	setLoading: (value: boolean) => void;
+	setUser: (value: UserDataType | undefined) => void;
+	setAvailableProviders: () => void;
+};
